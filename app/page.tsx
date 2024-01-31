4985c775ec6f9fda916dd6570d667b398ae77d57
Ashish Baravaliya <vier.institute35@gmail.com>
2024-01-31 10:39:25 -0500

    feat: view note + add button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,139 @@
+"use client";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+import { Plus } from "lucide-react";
+
+const defaultNotes = [
+  {
+    id: 1,
+    title: "Note 1",
+    note: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+    user: "Gurveer Singh",
+  },
+  {
+    id: 2,
+    title: "Week2 Delivery",
+    note: `
+    Your tasks this week include:
+    
+    • Develop a pseudocode algorithm for a collaborative note-taking application.
+    
+    • The application will need to support CRUD operations for creating, reading, updating,
+    and deleting notes.
+    
+    • Create a video where you detail the process of exactly how to develop the above task.
+    
+    • The video is used for instructional purposes by employees of Pinnacle Data Tech, so it needs to be comprehensive, engaging, and informative!
+    
+    • Be prepared to screen your creation at the end of the week.`,
+    user: "Rohit Sai",
+  },
+];
+
+interface Note {
+  id: number;
+  title: string;
+  note: string;
+  user: string;
+}
 
 export default function Home() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    const storedNotes = localStorage.getItem("notes");
+    if (storedNotes) {
+      setNotes(JSON.parse(storedNotes));
+    } else {
+      localStorage.setItem("notes", JSON.stringify(defaultNotes));
+    }
+  }, []);
+
+  const handleAddNote = () => {
+    const newNote = {
+      id: +new Date(),
+      title: "",
+      note: "",
+      user: "",
+    };
+    setNotes([...notes, newNote]);
+    localStorage.setItem("notes", JSON.stringify([...notes, newNote]));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex h-screen">
+      <div className="flex justify-between fixed inset-0 items-center h-16 px-6 border-b border-border bg-white shadow-md">
+        <div className=" text-lg  flex flex-1">
+          <Image src="/logo.png" alt="Logo" width={200} height={124} />
+        </div>
+        <div className="flex">
+          <Avatar className="h-10 w-10 shadow-md ">
+            <AvatarFallback className="bg-primary">AB</AvatarFallback>
+          </Avatar>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div
+        className="divWithDotsBackground flex-1 flex bg-white justify-center mt-16 py-6"
+        style={{
+          backgroundImage: `url('data:image/svg+xml;utf8,<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><circle cx="2" cy="2" r="1" fill="rgb(93, 23, 23)" /></svg>')`,
+        }}
+      >
+        <div className="flex flex-col gap-4 w-[800px] p-6 border-border border rounded-md justify-between bg-white shadow-md">
+          <div className="flex flex-col gap-4 flex-1">
+            <h1 className="text-3xl font-bold text-center">Notes</h1>
+            <div className="flex flex-col flex-1 h-full">
+              <div className="flex flex-wrap gap-5 mt-4 overflow-y-auto">
+                {notes.map((note, ind) => (
+                  <div
+                    key={ind}
+                    className="flex flex-col border border-border p-3 w-[220px] gap-2 bg-background rounded-[8px] shadow-md min-h-[235px]"
+                  >
+                    <p className="text-md font-bold">{note.title}</p>
+                    <div className="flex flex-1">
+                      <p
+                        className="text-xs text-black/50"
+                        style={{
+                          display: "-webkit-box",
+                          maxWidth: "200px",
+                          WebkitLineClamp: 10,
+                          WebkitBoxOrient: "vertical",
+                          overflow: " hidden",
+                        }}
+                      >
+                        {note.note}
+                      </p>
+                    </div>
+                    <div className="flex justify-between items-center pt-1">
+                      <div className="flex items-center">
+                        <Avatar className="h-6 w-6 shadow-sm">
+                          <AvatarFallback className="bg-primary text-xs">
+                            {note.user.split(" ")[0][0] +
+                              note.user.split(" ")[1][0]}
+                          </AvatarFallback>
+                        </Avatar>
+                        <p className="ml-1.5 text-sm">{note.user}</p>
+                      </div>
+                      <Button
+                        className="p-0 w-10 h-6 text-xs "
+                        variant="secondary"
+                      >
+                        View
+                      </Button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="flex justify-end">
+              <Button variant="secondary" onClick={handleAddNote}>
+                <Plus className="mr-2 h-4 w-4" /> Add Note
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
